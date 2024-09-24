e6030ecdc647e21e7704cf7c0528435ef9eabe34
Korey Miller <komille@siue.edu>
2024-09-24 13:09:33 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,16 @@
+Downloads
+-Ollama
+    Download Ollama for windows at Ollama.com/downloads/windows
+    Open OllamaSetup.exe
+    Follow instructions on OllamaSetup.exe and install Ollama
+
+IDE 
+    VS Code
+Running the code
+    Command prompt terminal
+        Run commands:
+            ollama serve (if not already run)
+            ollama run phi3 (if not already run)
+    VS code 
+        Run phi3.py
+        Responses will appear in a single line in Output.txt
---
+++ phi3.py
@@ -0,0 +1,17 @@
+import ollama
+with open("Input.txt", "r") as inputfile:                       #opens file containing the prompts
+    inputs = inputfile.readlines()                              #reads the prompts from the file
+for input in inputs:                                            #for loop for sending the prompts to phi3 
+    phi3response = ollama.chat(                                 #runs ollama locally
+        model="phi3",
+        messages=[
+        {
+            'role': 'user',
+            'content':input,
+        }],
+        stream = True)
+    for responses in phi3response:
+        with open("Output.txt", "a") as output:                 #opens file for the responses
+            output.write(responses['message']['content'])
+inputfile.close()                                               #closes file for the prompts 
+output.close()                                                  #closes file for the responses
